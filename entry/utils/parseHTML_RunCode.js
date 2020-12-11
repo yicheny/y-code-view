e7d220a5daa15efcf5709e0deedb156149a16ba6
yicheny <1463020554@qq.com>
2020-12-11 15:09:14 +0800

    fix jsx?

--- entry/utils/parseHTML_RunCode.js
+++ entry/utils/parseHTML_RunCode.js
@@ -7,7 +7,7 @@ export default function parseHTMLRunCode(source) {
 
     return {
         code,
-        beforeHTML:_.get(new RegExp(/([\s\S]+)<!--RunCode-->\s*(```js)([\s\S]+)(```)\s*<!--\/RunCode-->/gi).exec(source),'1',''),
-        afterHTML:_.get(new RegExp(/<!--RunCode-->\s*(```js)([\s\S]+)(```)\s*<!--\/RunCode-->([\s\S]+)/gi).exec(source),'4','')
+        beforeHTML:_.get(new RegExp(/([\s\S]+)<!--RunCode-->\s*(```jsx?)([\s\S]+)(```)\s*<!--\/RunCode-->/gi).exec(source),'1',''),
+        afterHTML:_.get(new RegExp(/<!--RunCode-->\s*(```jsx?)([\s\S]+)(```)\s*<!--\/RunCode-->([\s\S]+)/gi).exec(source),'4','')
     };
 }
